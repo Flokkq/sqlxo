0cea957a006dafd44f3c05f95414cb8dbe01dac6
Flokkq <webcla21@htl-kaindorf.at>
2025-06-14 20:06:01 +0200

    chore(repo): init
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Basic Rust flake for magazin api";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell rec {
+            buildInputs =
+              [
+                (rust-bin.selectLatestNightlyWith (toolchain:
+                  toolchain.default.override {
+                    extensions = ["rust-src" "rust-analyzer"];
+                    targets = [];
+                  }))
+
+                rustup
+
+                cargo-watch
+                cargo-tarpaulin
+                cargo-deny
+                clippy
+
+                # dev
+                pkg-config
+                openssl
+                cacert
+                sqlx-cli
+                postgresql_16
+                dotenvx
+              ]
+              ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
+                darwin.apple_sdk.frameworks.SystemConfiguration
+              ];
+
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+            RUSTUP_TOOLCHAIN = "nightly";
+            REQWEST_USE_RUSTLS = 1;
+            CURL_SSL_BACKEND = "secure_transport";
+            SWAGGER_UI_SKIP_SSL_CHECK = 1;
+          };
+        }
+    );
+}
