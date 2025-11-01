fd426700bba5cf85a42903933cc01a29b6378e72
Flokkq <webcla21@htl-kaindorf.at>
2025-11-01 16:17:09 +0100

    delete old tests
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- filter_macros/tests/try/err/filter_duplicate_key.rs
+++
@@ -1,9 +0,0 @@
-use filter_macros::Query;
-use sqlx::FromRow;
-
-#[derive(Debug, Clone, FromRow, Query)]
-#[filter(table_name = "a", table_name = "b")]
-pub struct T {
-    pub name: String,
-}
-fn main() {}
